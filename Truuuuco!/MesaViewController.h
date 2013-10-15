30e316e1f6db67755837200e59a7957739e86ff6
Gabriel Ribeiro <gribeiro@mobirama.com.br>
2013-10-15 07:52:39 -0300

    Labels e CMMotionManager

--- Truuuuco!/MesaViewController.h
+++ Truuuuco!/MesaViewController.h
@@ -7,7 +7,33 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 
-@interface MesaViewController : UIViewController
+@interface MesaViewController : UIViewController{
+    double currentMaxAccelX;
+    double currentMaxAccelY;
+    double currentMaxAccelZ;
+    double currentMaxRotX;
+    double currentMaxRotY;
+    double currentMaxRotZ;
+}
+
+@property (strong, nonatomic) IBOutlet UILabel *accX;
+@property (strong, nonatomic) IBOutlet UILabel *accY;
+@property (strong, nonatomic) IBOutlet UILabel *accZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxAccX;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccY;
+@property (strong, nonatomic) IBOutlet UILabel *maxAccZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *rotX;
+@property (strong, nonatomic) IBOutlet UILabel *rotY;
+@property (strong, nonatomic) IBOutlet UILabel *rotZ;
+
+@property (strong, nonatomic) IBOutlet UILabel *maxRotX;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotY;
+@property (strong, nonatomic) IBOutlet UILabel *maxRotZ;
+
+@property (nonatomic,strong) CMMotionManager *motionManager;
 
 @end
